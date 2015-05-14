2438fbf50f511e4dbfe223d62bcce98f4fa84dea
Taras <taras.horyachko@gmail.com>
2015-05-14 10:32:57 +0300

    first commit

---
+++ Bowling/Models/Frame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling.Models
+{
+    public class Frame
+    {
+        public int first { set; get; }
+        public int second { set; get; }
+    }
+}
---
+++ Bowling/Scripts/app.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('bowlingApp', []);
+
+app.controller('ScoreController', ['$http', function ($http) {
+    var score = this;
+    score.newFrame = {}
+    score.match = { frames: [] };
+    score.total = 0;
+
+    score.addNewFrame = function () {
+        var newFrames = score.match.frames.slice();
+        newFrames.push(score.newFrame);
+
+        $http.post('/Main/Count', { frames: newFrames }).
+          success(function (data) {
+              score.total = data.score;
+              score.match.frames.push(score.newFrame);
+              score.newFrame = {};
+          }).
+          error(function (data) {
+          });
+
+    };
+}]);
