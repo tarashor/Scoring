abac15364b317df058d0a59a20d0a36e97d39737
Taras <taras.horyachko@gmail.com>
2015-05-14 17:24:45 +0300

    add client validation

--- Bowling/Scripts/app.js
+++ Bowling/Scripts/app.js
@@ -8,26 +8,27 @@ function createNewFrame()
 app.controller('ScoreController', ['$http', function ($http) {
     var score = this;
     score.MAX_FRAMES = 10;
+    score.PINS_IN_FRAME = 10;
     score.newFrame = createNewFrame();
-    score.match = { frames: [] };
+    score.frames = [];
     score.total = 0;
 
     score.addNewFrame = function () {
-        var newFrames = score.match.frames.concat(score.newFrame);
+        if (score.validateNewFrame()) {
+            var newFrames = score.frames.concat(score.getNewFrame());
 
-        $http.post('/Main/Count', { frames: newFrames }).
-          success(function (data) {
-              score.total = data.score;
-              score.match.frames.push(score.newFrame);
-              score.newFrame = createNewFrame();
-          }).
-          error(function (data) {
-              alert(data.message);
-          });
-    };
-
-    score.isLastFrame = function () {
-        return score.match.frames.length === (score.MAX_FRAMES - 1);
+            $http.post('/Main/Count', { frames: newFrames }).
+              success(function (data) {
+                  score.total = data.score;
+                  score.frames.push(score.getNewFrame());
+                  score.newFrame = createNewFrame();
+              }).
+              error(function (data) {
+                  alert(data.message);
+              });
+        } else {
+            alert("Input values is not correct!!");
+        }
     };
 
     score.isStrike = function (frame) {
@@ -38,14 +39,48 @@ app.controller('ScoreController', ['$http', function ($http) {
         return (frame.first + frame.second) === 10;
     };
 
+    score.canRollSecond = function () {
+        return !score.isStrike(score.newFrame) || score.isLastFrame();
+    };
+
+    score.hasSecondRoll = function (frame) {
+        return !score.isStrike(frame) || (score.frames.indexOf(frame) < score.MAX_FRAMES - 1);
+    };
+
     score.canRollThird = function () {
         return score.isLastFrame() && (score.isSpare(score.newFrame) || score.isStrike(score.newFrame));
     };
 
-    score.gameFinished = function () {
-        return score.match.frames.length === score.MAX_FRAMES;
+    score.hasThirdRoll = function (frame) {
+        return (score.frames.indexOf(frame) === score.MAX_FRAMES - 1) && (score.isSpare(frame) || score.isStrike(frame));
     };
 
-    
+    score.isLastFrame = function () {
+        return score.frames.length === (score.MAX_FRAMES - 1);
+    };
+
+    score.gameFinished = function () {
+        return score.frames.length === score.MAX_FRAMES;
+    };
+
+    score.getNewFrame = function () {
+        if (score.canRollThird())
+            return score.newFrame;
+        else
+            return { first: score.newFrame.first, second: score.newFrame.second };
+    };
+
+    score.validateNewFrame = function () {
+        if (!score.canRollThird()) {
+            return (score.newFrame.first + score.newFrame.second <= score.PINS_IN_FRAME) && (score.newFrame.third === 0);
+        }
+        else {
+            if (score.isSpare(score.newFrame)) {
+                return (score.newFrame.first + score.newFrame.second <= score.PINS_IN_FRAME) && (score.newFrame.third <= score.PINS_IN_FRAME);
+            } else if (score.isStrike(score.newFrame)) {
+                return (score.newFrame.first <= score.PINS_IN_FRAME) && (score.newFrame.second <= score.PINS_IN_FRAME) && (score.newFrame.third <= score.PINS_IN_FRAME);
+            }
+        }
+    };
 
 }]);
