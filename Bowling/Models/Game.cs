13341cd6ddda1b7bb04e8b48143856e4c5853cdc
Taras <taras.horyachko@gmail.com>
2015-05-14 19:42:02 +0300

    add serverside validation

---
+++ Bowling/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using Bowling.Models.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bowling.Models
+{
+    [BowlingGameValidation(Pins = 10, MaxFrames = 10)]
+    public class Game
+    {
+        public Game() {
+            frames = new List<Frame>();
+        }
+        public IList<Frame> frames { get; private set; }
+    }
+}
---
+++ Bowling/Models/Validation/BowlingGameValidation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bowling.Models.Validation
+{
+    public class BowlingGameValidation : ValidationAttribute
+    {
+        public int Pins { get; set; }
+        public int MaxFrames { get; set; }
+
+        public override bool IsValid(object value)
+        {
+            bool isValid = false;
+            Game game = value as Game;
+            if (game != null)
+            {
+                if (game.frames.Count <= MaxFrames)
+                {
+                    for (int i = 0; i < game.frames.Count; i++)
+                    {
+                        Frame frame = game.frames[i];
+                        if (i == MaxFrames - 1)
+                        {
+                            if (frame.first == Pins) 
+                            {
+                                //Strike
+                            }
+                            else if ((frame.first + frame.second) == Pins) 
+                            { 
+                                //Spare
+                            }
+                        }
+
+                        bool isFrameValid = ((frame.first + frame.second) <= Pins) && (frame.first >= 0) && (frame.second >= 0) && (frame.third == 0);
+
+                    }
+                }
+            }
+            return isValid;
+
+        }
+    }
+}
